4768723a7142848324bd8ceadbe26d9af07cadf1
MaVirgil <magnusvirgil@hotmail.com>
2026-09-21 23:36:00 +0200

    move constants to seperate file

--- components/Note.js
+++ components/Note.js
@@ -1,9 +1,9 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { useNavigation } from "@react-navigation/core";
 import TextButton from "./TextButton";
+import { NOTE_PREVIEW_LENGTH_LIMIT } from "../lib/constants";
 
 export default function Note({ note, onSave, onDelete }) {
-  const NOTE_PREVIEW_LENGTH_LIMIT = 25;
   const navigation = useNavigation();
 
   console.log(`Note component recieved note object: ${JSON.stringify(note)}`)
--- views/NoteDetails.js
+++ views/NoteDetails.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
 import PageWrapper from '../components/PageWrapper';
 import Button from '../components/Button';
+import { NOTE_MAX_LENGTH } from '../lib/constants';
 import { useState } from 'react';
 
 export default function NoteDetails({ route }) {
@@ -10,12 +11,13 @@ export default function NoteDetails({ route }) {
 
   return (
     <PageWrapper>
-      <View>
+      <View style={{ marginVertical: 20 }}>
         <TextInput
+          style={styles.noteEditField}
           multiline={true}
+          maxLength={NOTE_MAX_LENGTH}
           onChangeText={(value) => setNote((prev) => ({...prev, text: value}))}
           value={note.text}
-
         >
         </TextInput>
       </View>
@@ -29,6 +31,11 @@ export default function NoteDetails({ route }) {
 
 const styles = StyleSheet.create({
   noteEditField: {
-
+    backgroundColor: "#ebf9ff",
+    borderRadius: 15,
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    width: 300,
+    height: 190,
   }
 });
